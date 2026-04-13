23076bb4b9895be55f68de84b0dbc10866c9954c
Giri <girih9033@gmail.com>
2026-04-13 14:00:18 +0530

    quiz app

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connect
+mongoose.connect("mongodb+srv://ultrabot805_db_user:giri123@cluster0.0dsdrtc.mongodb.net/quizdb")
+  .then(() => console.log("MongoDB Atlas Connected ✅"))
+  .catch(err => console.log(err));
+
+// Schema
+const Leaderboard = mongoose.model("Leaderboard", {
+  name: String,
+  score: Number,
+  total: Number,
+  date: String
+});
+
+// Save API
+app.post("/save", async (req, res) => {
+  const data = new Leaderboard(req.body);
+  await data.save();
+  res.json({ message: "Saved" });
+});
+
+// Get API
+app.get("/leaderboard", async (req, res) => {
+  const data = await Leaderboard.find().sort({ score: -1 });
+  res.json(data);
+});
+
+app.listen(5000, () => console.log("Server running 🚀"));
